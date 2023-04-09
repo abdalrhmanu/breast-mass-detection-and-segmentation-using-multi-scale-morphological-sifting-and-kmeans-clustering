3a05226b9d74f2587a5c72dd0c40f4d711fa797c
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-04-10 01:27:22 +0300

    thresholding and finding contours, cropping image preprocessing

---
+++ helpers/display.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+def plot_figures(figures, nrows = 1, ncols=1):
+    """Plot a dictionary of figures.
+
+    Parameters
+    ----------
+    figures : <title, figure> dictionary
+    ncols : number of columns of subplots wanted in the display
+    nrows : number of rows of subplots wanted in the figure
+    """
+
+    print(figures.keys())
+    fig, axeslist = plt.subplots(ncols=ncols, nrows=nrows, figsize=(18, 6))
+    for ind,title in zip(range(len(figures)), figures):
+        axeslist.ravel()[ind].imshow(figures[title], cmap='gray')
+        axeslist.ravel()[ind].set_title(title)
+        axeslist.ravel()[ind].set_axis_off()
+    plt.tight_layout() # optional
